9bccaaa52eac85cbe38e988f833343c5b933c180
Ridhi Singla <singlaridhi2007@.com>
2026-08-17 17:39:00 +0530

    Today's Classwork

--- LinkedListcount.java
+++ LinkedListcount.java
@@ -21,11 +21,11 @@ public class LinkedListcount {
         return count;
     }
 
-    public static void main(String[] args) {
+    
+}public static void main(String[] args) {
         Node head = new Node(10);
         head.next=new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
         System.out.println(countNodes(head));
     }
-}
